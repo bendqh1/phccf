1499c79339a575adf25f1b8a29b6674c1c62a485
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-11 21:00:47 +0700

    Delete 1_confirmation.php

--- 1_confirmation.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if(isset($_REQUEST["destination"])){
-		header("Location: {$_REQUEST["destination"]}");
-	}else if(isset($_SERVER["HTTP_REFERER"])){
-		header("Location: {$_SERVER["HTTP_REFERER"]}");
-	}else{
-		/* some fallback, maybe redirect to index.php */
-	}
-?>
