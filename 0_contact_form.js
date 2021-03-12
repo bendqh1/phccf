7dc3ff3afa59ad1724caae32f04822af6770fd65
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-12 17:32:15 +0700

    Update 0_contact_form.js

--- 0_contact_form.js
+++ 0_contact_form.js
@@ -1,11 +1,11 @@
 // Select contact form:
 const contact_form = document.querySelector("#contact_form");
 
-// A function to call on(the)Submit event:
+// A function to call, on(the)Submit event:
 function passedProcedure() {
 	alert("לחץ לחזרה לדף הקודם");
 	window.location.replace("http://walla.co.il");
 }
 
-// On(the)Submit event call: passedProcedure():
+// On(the)Submit event call passedProcedure():
 contact_form.addEventListener("submit", passedProcedure) 
