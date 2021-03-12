922a91ae252781e0f61fa5d853c5f48ab5899ac3
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-12 19:18:03 +0700

    Update 0_contact_form.php

--- 0_contact_form.php
+++ 0_contact_form.php
@@ -1,9 +1,26 @@
 <?php
-	$to = "YOUR_EMAIL_ADDRESS_NAME@YOUR_EMAIL_ADDRESS_DELIMITER";
-	$headers = $email = $_POST['email'] .  "\r\n";
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$phone = $_POST["phone"];
+	$topic = $_POST["topic"];
+	$date = $_POST["date"];
+	$notes = $_POST["notes"];
 
-	$subject = "פנייה מטופס יצירת קשר מאת" . $name = $_POST['name'];
-	$message = $phone = $_POST['phone'] . "\r\n" . $topic = $_POST['topic'] . "\r\n" . $date = $_POST['date'] . "\r\n" . $notes = $_POST['notes'];
+	$to = "example@example.com"; ########## !!! NOTE !!! ##########
+	$subject = "הודעת יצירת קשר מאת" . $name;
+	$message = array (
+		$name = "שם" . "\r\n",
+		$name = "אימייל" . "\r\n",
+		$phone = "טלפון" . "\r\n",
+		$topic = "פנייה בנושא" . "\r\n",
+		$date = "תאריך רצוי לפגישה (אם יש)" . "\r\n",
+		$notes = "הערות (אם יש)" . "\r\n"
+	);
+	$headers = array(
+		"From" => $email,
+		"Reply-To" => $email
+	);
+	# The order should be as above: $to, $subject, $message and $headers
 	
-	mail($to, $headers, $subject, $message);
+	mail($to, $subject, $message, $headers);
 ?>
