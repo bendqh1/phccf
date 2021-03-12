b906e87a877503d3f69d056d04673693a50e5283
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-12 16:02:07 +0700

    Update 0_contact_form.php

--- 0_contact_form.php
+++ 0_contact_form.php
@@ -8,7 +8,7 @@
 
 	$to = "YOUR_EMAIL_ADDRESS_NAME@YOUR_EMAIL_ADDRESS_DELIMITER";
 	$headers = "From: $email \r\n";
-	$subject = "פנייה מטופס יצירת קשר" . "From: $name ";
+	$subject = "פנייה מטופס יצירת קשר מאת" . $name;
 	$message = $phone . echo htmlspecialchars("<br>") . $topic . echo htmlspecialchars("<br>") . $notes
 	
 	mail($to, $headers, $subject, $message) or die("Error!");
