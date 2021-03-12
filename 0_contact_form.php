cb3ed242586890bef236a3ae568867ecf4ecc432
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-12 16:01:25 +0700

    Update 0_contact_form.php

--- 0_contact_form.php
+++ 0_contact_form.php
@@ -2,13 +2,14 @@
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$phone = $_POST['phone'];
-	$phone = $_POST['topic'];
-	# $phone = $_POST['date'];
+	$topic = $_POST['topic'];
+	# $date = $_POST['date'];
 	$notes = $_POST['notes'];
 
-	$recipient = "YOUR_EMAIL_ADDRESS_NAME@EXAMPLE.COM";
-	$subject = "פנייה מטופס יצירת קשר";
-	$mailheader = "From: $email \r\n";
-
-	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+	$to = "YOUR_EMAIL_ADDRESS_NAME@YOUR_EMAIL_ADDRESS_DELIMITER";
+	$headers = "From: $email \r\n";
+	$subject = "פנייה מטופס יצירת קשר" . "From: $name ";
+	$message = $phone . echo htmlspecialchars("<br>") . $topic . echo htmlspecialchars("<br>") . $notes
+	
+	mail($to, $headers, $subject, $message) or die("Error!");
 ?>
