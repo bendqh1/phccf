0f4560f613371818c430ff9347bbdddc84c6ef0e
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-19 21:04:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,15 @@ cd "${website_dir}" &&
 rm -rf prcf-main &&
 curl -L -O https://github.com/bendqh1/prcf/archive/main.zip &&
 unzip main.zip &&
-rm main.zip
-nano prcf-main/0_contact_form.php
+rm main.zip &&
+
+while true; do
+    read -p "Do you wish to configure your email address for the form in the php file?" yn
+    case $yn in
+        [Yy]* ) nano prcf-main/0_contact_form.php; ##break;;
+        [Nn]* ) break #exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 # In the `curl` above `=L` is to follow a redirection in which case happens and `-O` is to keep the remote name (instead getting a generic file name).
