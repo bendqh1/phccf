a67d0d0fa7be277b32b7d9bb92b71c515d561bf4
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-19 21:00:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# I suggested to save your website direcotry path into a global variable and then do tasks;
+# If you have just one website directory per system, I suggest to save that website direcotry path into a global variable and then do tasks:
 
 cd "${website_dir}" &&
 rm -rf prcf-main &&
