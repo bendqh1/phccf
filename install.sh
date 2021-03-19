03b40da957182b40d2abb22e9c9a16972376e4f4
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-19 21:42:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ rm -rf prcf-main &&
 curl -L -O https://github.com/bendqh1/prcf/archive/main.zip &&
 unzip main.zip &&
 rm main.zip &&
-
 while true; do
     read -p "Do you wish to configure your email address for the form in the php file?" yn
     case $yn in
