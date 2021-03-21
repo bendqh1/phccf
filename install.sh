57701394c9d898782be2ccb844d3d622d08d1f07
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-21 15:47:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ curl -L -O https://github.com/bendqh1/prcf/archive/main.zip
 unzip main.zip
 rm main.zip
 
-while true; do
-    read -p "Do you wish to configure your email address for the form in the php file now?" yn
-    case $yn in
-        [Yy]* ) nano prcf-main/0_contact_form.php; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+# while true; do
+    # read -p "Do you wish to configure your email address for the form in the php file now?" yn
+    # case $yn in
+        # [Yy]* ) nano prcf-main/0_contact_form.php; break;;
+        # [Nn]* ) break;;
+        # * ) echo "Please answer yes or no.";;
+    # esac
+# done
