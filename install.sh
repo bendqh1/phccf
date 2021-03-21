bf2ea2d7b8ffd34d004e7fd46cbdd8aa0ead7d44
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-21 18:10:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ rm main.zip
 mv "${website_dir}"/prcf-main/html/contact_form.html "${website_dir}"/contact_form.html
 mv "${website_dir}"/prcf-main/html/contact_form_success.html "${website_dir}"/contact_form_success.html
 
-# while true; do
-    # read -p "Do you wish to configure your email address for the form in the php file now?" yn
-    # case $yn in
-        # [Yy]* ) nano prcf-main/0_contact_form.php; break;;
-        # [Nn]* ) break;;
-        # * ) echo "Please answer yes or no.";;
-    # esac
-# done
+while true; do
+    read -p "Do you wish to configure your email address for the form in the php file now?" yn
+    case $yn in
+        [Yy]* ) nano prcf-main/0_contact_form.php; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
