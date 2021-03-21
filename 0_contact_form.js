06fd5260344c75d920ea50fcc8bf63e9a25c4ec7
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-21 17:02:53 +0700

    Update 0_contact_form.js

--- 0_contact_form.js
+++ 0_contact_form.js
@@ -1,20 +1,3 @@
 // Select contact form:
 const contact_form = document.querySelector("#contact_form");
-
-const labelAsterisks = document.qeurySelectorAll("label");
-labelAsterisks.forEach((element) => {
-    if (element.textContent.indexOf("*") !== -1) {
-        element.textContent.indexOf.style.color = "Red";
-    }
-});
-
-/*
-	// A function to call, on(the)Submit event:
-	function passedProcedure() {
-		alert("לחץ לחזרה לדף הקודם");
-		window.location.replace("http://walla.co.il");
-	}
-
-	// On(the)Submit event call passedProcedure():
-	contact_form.addEventListener("submit", passedProcedure)
-*/
+const success = document.querySelector("success");
