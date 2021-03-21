18360922765c368969d99f7e9270f6aa7fa648bb
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-21 17:13:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ curl -L -O https://github.com/bendqh1/prcf/archive/main.zip
 unzip main.zip
 rm main.zip
 mv /prcf-main/html/contact_form.html . # MOVE THE FILE to site dir
-mv /prcf-main/html/success.html . # MOVE THE FILE to site dir
+mv /prcf-main/html/contact_form_success.html . # MOVE THE FILE to site dir
 
 # while true; do
     # read -p "Do you wish to configure your email address for the form in the php file now?" yn
