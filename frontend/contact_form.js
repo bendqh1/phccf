d918f85bca9e309258cb4a7f64cedde711a6400d
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-26 12:18:35 +0700

    Update contact_form.js

--- frontend/contact_form.js
+++ frontend/contact_form.js
@@ -3,14 +3,8 @@ const contact_form = document.querySelector("#contact_form");
 
 // Replace contact form with success message:
 
-const success = document.createElement('h2')
-success.innerHTML = `
-<div id="success" class="general_structure success">
-תודה על ההתעניינות לעבוד עימי
-על כל פנייה בנושאי עבודה או נגישות אשתדל להשיב בהקדם
-בברכה
-</div>
-`
-
-contact_form.replaceWith(success);
+const success = document.createElement('div')
+success.setAttribute("id", "success");
+success.classList.add("general_structure", "success");
+success.innerHTML = "תודה על ההתעניינות לעבוד עימי,<br>על כל פנייה בנושאי עבודה או נגישות אשתדל להשיב בהקדם<br>בברכה,"
 
