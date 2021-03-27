cc9c05a9f204ce469e8c3a6cc3432e32e1747edf
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-27 19:48:18 +0700

    Update contact_form.php

--- backend/contact_form.php
+++ backend/contact_form.php
@@ -1,13 +1,22 @@
 <?php
+
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$phone = $_POST["phone"];
+	$topic = $_POST["topic"];
+	$date = $_POST["date"];
+	$time = $_POST["time"];
+	$notes = $_POST["notes"];
+
 	$to = "example@example.com";
-	$subject = "New email message";
-	$message = $name = $_POST["name"] . "שם: " . "\r\n"
-		. $email = $_POST["email"] . "אימייל: " . "\r\n"
-		. $phone = $_POST["phone"] . "טלפון: " . "\r\n"
-		. $topic = $_POST["topic"] . "פנייה בנושא: " . "\r\n"
-		. $date = $_POST["date"] . "יום רצוי לפגישה מרחוק (אם יש): " . "\r\n"
-		. $time = $_POST["time"] . "שעה רצויה לפגישה מרחוק (אם יש):" . "\r\n"
-		. $notes = $_POST["notes"] . "הערות (אם יש):" . "\r\n"; # Note the semicolon before this comment;
+	$subject =  $name . "הודעת אימייל חדשה מאת: ";
+	$message = $name . "שם: " . "\r\n"
+		. $email . "אימייל: " . "\r\n"
+		. $phone . "טלפון: " . "\r\n"
+		. $topic . "פנייה בנושא: " . "\r\n"
+		. $date . "יום רצוי לפגישה מרחוק (אם יש): " . "\r\n"
+		. $time . "שעה רצויה לפגישה מרחוק (אם יש):" . "\r\n"
+		. $notes . "הערות (אם יש):" . "\r\n"; # Note the semicolon before this comment;
 
 	mail($to, $subject, $message);
 	header('Content-Type: text/html; charset=utf-8');
