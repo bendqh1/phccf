f00d85cbfe71ea5321e86ab9e5df4dee1ad37c82
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-27 14:48:43 +0700

    Update contact_form.php

--- backend/contact_form.php
+++ backend/contact_form.php
@@ -1,22 +1,22 @@
 <?php
 	$contact_form_url="example.com/contact";
+	$contact_form_success_url="example.com/contact_succees";
 
 	$to = "example@example.com";
 	$subject = "New email message";
-	$message = array (
-		$name = $_POST["name"] . "Name:" . "\r\n",
-		$email = $_POST["email"] . "Email:" . "\r\n",
-		$phone = $_POST["phone"] . "Phone:" . "\r\n",
-		$topic = $_POST["topic"] . "Topic:" . "\r\n",
-		$date = $_POST["date"] . "Date:" . "\r\n",
-		$time = $_POST["time"] . "Time:" . "\r\n",
-		$notes = $_POST["notes"] . "Notes:" . "\r\n"
+	$message = $name = $_POST["name"] . "Name:" . "\r\n"
+		. $email = $_POST["email"] . "Email:" . "\r\n"
+		. $phone = $_POST["phone"] . "Phone:" . "\r\n"
+		. $topic = $_POST["topic"] . "Topic:" . "\r\n"
+		. $date = $_POST["date"] . "Date:" . "\r\n"
+		. $time = $_POST["time"] . "Time:" . "\r\n"
+		. $notes = $_POST["notes"] . "Notes:" . "\r\n"
 	);
 
 	mail($to, $subject, $message);
 
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
- 		header("Location: $contact_form_url");
+ 		header("Location: $contact_form_success_url");
 		exit;
 	}
 ?>
