94843506a13708744d6ccb88253adc1a826d50c9
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-27 18:28:19 +0700

    Update contact_form.php

--- backend/contact_form.php
+++ backend/contact_form.php
@@ -10,8 +10,7 @@
 		. $topic = $_POST["topic"] . "Topic:" . "\r\n"
 		. $date = $_POST["date"] . "Date:" . "\r\n"
 		. $time = $_POST["time"] . "Time:" . "\r\n"
-		. $notes = $_POST["notes"] . "Notes:" . "\r\n"
-	);
+		. $notes = $_POST["notes"] . "Notes:" . "\r\n"; # Note the semicolon before this comment;
 
 	mail($to, $subject, $message);
 
