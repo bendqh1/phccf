8fbe5aef61c3b05386f39060798924c0b8623466
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-28 22:09:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,3 @@ rm -rf prcf-main
 curl -L -O https://github.com/bendqh1/prcf/archive/main.zip
 unzip main.zip
 rm main.zip
-
-while true; do
-    read -p "Do you wish to configure php variables now?" yn
-    case $yn in
-        [Yy]* ) nano prcf-main/backend/contact_form.php; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
