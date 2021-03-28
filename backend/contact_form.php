0d18db1b4d91f530abd7a5abeb596a4cc8b20242
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-28 14:30:41 +0700

    Update contact_form.php

--- backend/contact_form.php
+++ backend/contact_form.php
@@ -8,8 +8,22 @@
 	$notes = $_POST["notes"];
 
 	$to = "example@example.com";
+
 	$subject = "הודעת אימייל חדשה מאת: " . $name;
-	include "message_variable.php";
+	
+	$message = "
+	$message .= "<html>"
+	$message .= <body dir="rtl" style="text-align: right;">
+	$message .= <b>"שם: </b>" . $name . "\r\n"
+	$message .= <b>"אימייל: </b>" . $email . "\r\n"
+	$message .= <b>"טלפון: </b>" . $phone . "\r\n"
+	$message .= <b>"פנייה בנושא: </b>" . $topic . "\r\n"
+	$message .= <b>"יום רצוי לפגישה מרחוק (אם יש): </b>" . $date . "\r\n"
+	$message .= <b>"שעה רצויה לפגישה מרחוק (אם יש): </b>" . $time . "\r\n"
+	$message .= <b>"הערות (אם יש): </b>" . $notes . "\r\n";
+	$message .= </body>
+	$message .= </html>
+	";
 
 	mail($to, $subject, $message);
 	header("'MIME-Version: 1.0");
