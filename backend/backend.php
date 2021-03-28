e9c8e7e85dc825c1113b5e878cb0a19f4f3ee5a6
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-28 22:05:26 +0700

    Update backend.php

--- backend/backend.php
+++ backend/backend.php
@@ -15,12 +15,12 @@
 	$message = <<<LABEL
 	<html>
 		<head>
-			<link rel="stylesheet" href="backend.css" type="text/css">
+			<link rel="stylesheet" href="prcf-main/backend/backend.css" type="text/css"></link>
 		</head>
 		<body dir="rtl" style="text-align: right;">
 			<p class="form_output_col_1">שם</p><p class="form_output_col_2">$name</p>
-			<p class="form_output_col_1">אימייל</p><p class="form_output_col_2">$email</p>
-			<p class="form_output_col_1">טלפון</p><p class="form_output_col_2">$phone</p>
+			<p class="form_output_col_1">אימייל</p><p id="form_output_email" class="form_output_col_2">$email</p>
+			<p class="form_output_col_1">טלפון</p><p id="form_output_phone" class="form_output_col_2">$phone</p>
 			<p class="form_output_col_1">פנייה בנושא<p class="form_output_col_2">$topic</p>
 			<p class="form_output_col_1">דומיין אתר (אםיש)</p><p class="form_output_col_2">$url</p>
 			<p class="form_output_col_1">יום רצוי לפגישה מרחוק (אם יש)</p><p class="form_output_col_2">$date</p>
