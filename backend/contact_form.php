7c76441232cfd4a1685a6d053f10ab6e83ee658e
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-28 13:13:06 +0700

    Update contact_form.php

--- backend/contact_form.php
+++ backend/contact_form.php
@@ -9,13 +9,14 @@
 
 	$to = "example@example.com";
 	$subject = "הודעת אימייל חדשה מאת: " . $name;
-	$message = "<b>שם: </b>" . $name . "\r\n"
-		. "<b>אימייל: </b>" . $email . "\r\n"
-		. "<b>טלפון: </b>" . $phone . "\r\n"
-		. "<b>פנייה בנושא: </b>" . $topic . "\r\n"
-		. "<b>יום רצוי לפגישה מרחוק (אם יש): </b>" . $date . "\r\n"
-		. "<b>שעה רצויה לפגישה מרחוק (אם יש): </b>" . $time . "\r\n"
-		. "<b>הערות (אם יש): </b>" . $notes . "\r\n";
+
+	$message = echo "<b>שם: </b>" . $name . "\r\n"
+		. echo "<b>אימייל: </b>" . $email . "\r\n"
+		. echo "<b>טלפון: </b>" . $phone . "\r\n"
+		. echo "<b>פנייה בנושא: </b>" . $topic . "\r\n"
+		. echo "<b>יום רצוי לפגישה מרחוק (אם יש): </b>" . $date . "\r\n"
+		. echo "<b>שעה רצויה לפגישה מרחוק (אם יש): </b>" . $time . "\r\n"
+		. echo "<b>הערות (אם יש): </b>" . $notes . "\r\n";
 
 	mail($to, $subject, $message);
 	header('Content-Type: text/html; charset=utf-8');
