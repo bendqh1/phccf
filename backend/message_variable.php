1dc90bf525208244566f92828043f190b3f0259c
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-28 14:21:54 +0700

    Create message_variable.php

---
+++ backend/message_variable.php
@@ -0,0 +1,16 @@
+	
+
+
+$message = "
+	<html>
+		<body dir="rtl" style="text-align: right;">
+			. <b>"שם: </b>" . $name . "\r\n"
+			. <b>"אימייל: </b>" . $email . "\r\n"
+			. <b>"טלפון: </b>" . $phone . "\r\n"
+			. <b>"פנייה בנושא: </b>" . $topic . "\r\n"
+			. <b>"יום רצוי לפגישה מרחוק (אם יש): </b>" . $date . "\r\n"
+			. <b>"שעה רצויה לפגישה מרחוק (אם יש): </b>" . $time . "\r\n"
+			. <b>"הערות (אם יש): </b>" . $notes . "\r\n";
+		</body>
+	</html>
+";
