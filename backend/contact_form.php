7e958f0cc1476b729478ba405c20af3b0a92d8c9
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-28 14:56:16 +0700

    Update contact_form.php

--- backend/contact_form.php
+++ backend/contact_form.php
@@ -13,19 +13,23 @@
 	
 	$message = '
 	<html>
-	<body dir="rtl" style="text-align: right;">
-	<b>שם: </b> . $name . <br>
-	<b>אימייל: </b> . $email . <br>
-	<b>טלפון: </b> . $phone . <br>
-	<b>פנייה בנושא: </b> . $topic . <br>
-	<b>יום רצוי לפגישה מרחוק (אם יש): </b> . $date . <br>
-	<b>שעה רצויה לפגישה מרחוק (אם יש): </b> . $time . <br>
-	<b>הערות (אם יש): </b> . $notes . <br>
-	</body>
+		<head>
+  			<title>RTL contact form message</title>
+		</head>
+		<body dir="rtl" style="text-align: right;">
+			<b>שם: </b> . $name . <br>
+			<b>אימייל: </b> . $email . <br>
+			<b>טלפון: </b> . $phone . <br>
+			<b>פנייה בנושא: </b> . $topic . <br>
+			<b>יום רצוי לפגישה מרחוק (אם יש): </b> . $date . <br>
+			<b>שעה רצויה לפגישה מרחוק (אם יש): </b> . $time . <br>
+			<b>הערות (אם יש): </b> . $notes . <br>
+		</body>
 	</html>
 	';
 
-	mail($to, $subject, $message);
-	header('MIME-Version: 1.0');
-	header('Content-Type: text/html; charset=utf-8');
+	$headers[] = 'MIME-Version: 1.0';
+	$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+	mail($to, $subject, $message, implode("\r\n", $headers));
 ?>
