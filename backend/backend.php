e932e01302f653e9f661f35591a20ea500892489
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-28 22:27:13 +0700

    Update backend.php

--- backend/backend.php
+++ backend/backend.php
@@ -17,8 +17,8 @@
 		<head>
 			<style>
 			* {
-				font-size: 16px;
-				display: inline;
+				font-size: 18px;
+				display: inline-block;
 			}
 			.form_output_col_1 {
 				font-weight: bold;
@@ -33,7 +33,7 @@
 			<p class="form_output_col_1">אימייל</p><p dir="ltr" class="form_output_col_2">$email</p>
 			<p class="form_output_col_1">טלפון</p><p dir="ltr" class="form_output_col_2">$phone</p>
 			<p class="form_output_col_1">פנייה בנושא<p class="form_output_col_2">$topic</p>
-			<p class="form_output_col_1">דומיין אתר (אםיש)</p><p dir="ltr" class="form_output_col_2">$url</p>
+			<p class="form_output_col_1">דומיין אתר (אם יש)</p><p dir="ltr" class="form_output_col_2">$url</p>
 			<p class="form_output_col_1">יום רצוי לפגישה מרחוק (אם יש)</p><p class="form_output_col_2">$date</p>
 			<p class="form_output_col_1">שעה רצויה לפגישה מרחוק (אם יש)</p><p class="form_output_col_2">$time</p>
 			<p class="form_output_col_1">הערות (אם יש)</p><p class="form_output_col_2">$notes</p>
