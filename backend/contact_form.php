e8a94600a0e9c72e167d7b4dcf65e9699db1da1b
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-28 16:13:07 +0700

    Update contact_form.php

--- backend/contact_form.php
+++ backend/contact_form.php
@@ -25,10 +25,6 @@
 	</html>
 	';
 
-	$message = ' 
-	<body dir="rtl" style="text-align: right;"><b>שם: </b></body></html> $name;
-	';
-
 	$headers[] = 'MIME-Version: 1.0';
 	$headers[] = 'Content-type: text/html; charset=utf-8';
 
