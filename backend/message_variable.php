d244cb5c61604a8fdfb653116befa09d46dbed66
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-28 14:28:15 +0700

    Update message_variable.php

--- backend/message_variable.php
+++ backend/message_variable.php
@@ -1,16 +1,13 @@
-	
-
-
 $message = "
-	<html>
-		<body dir="rtl" style="text-align: right;">
-			. <b>"שם: </b>" . $name . "\r\n"
-			. <b>"אימייל: </b>" . $email . "\r\n"
-			. <b>"טלפון: </b>" . $phone . "\r\n"
-			. <b>"פנייה בנושא: </b>" . $topic . "\r\n"
-			. <b>"יום רצוי לפגישה מרחוק (אם יש): </b>" . $date . "\r\n"
-			. <b>"שעה רצויה לפגישה מרחוק (אם יש): </b>" . $time . "\r\n"
-			. <b>"הערות (אם יש): </b>" . $notes . "\r\n";
-		</body>
-	</html>
+$message .= "<html>"
+$message .= <body dir="rtl" style="text-align: right;">
+$message .= <b>"שם: </b>" . $name . "\r\n"
+$message .= <b>"אימייל: </b>" . $email . "\r\n"
+$message .= <b>"טלפון: </b>" . $phone . "\r\n"
+$message .= <b>"פנייה בנושא: </b>" . $topic . "\r\n"
+$message .= <b>"יום רצוי לפגישה מרחוק (אם יש): </b>" . $date . "\r\n"
+$message .= <b>"שעה רצויה לפגישה מרחוק (אם יש): </b>" . $time . "\r\n"
+$message .= <b>"הערות (אם יש): </b>" . $notes . "\r\n";
+$message .= </body>
+$message .= </html>
 ";
