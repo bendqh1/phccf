d2b7ee838e7ce09d18ef43a46a0b0e32dc34bede
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-29 22:02:52 +0700

    Update frontend.js

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,13 +1,11 @@
-document.addEventListener("load", myScript);
-
-function myScript() {
-let contactForm = document.querySelector("#contact_form")
-contactForm.addEventListener("submit", function(event){
-	event.preventDefault()
-	var xhr = new XMLHttpRequest();
-	xhr.open(contactForm.method, contactForm.action, true);
-	xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
-	xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-	xhr.send();
-});
-};
+setTimeout( ()=> {
+	let contactForm = document.querySelector("#contact_form")
+	contactForm.addEventListener("submit", function(event){
+		event.preventDefault()
+		var xhr = new XMLHttpRequest();
+		xhr.open(contactForm.method, contactForm.action, true);
+		xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
+		xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+		xhr.send();
+	});
+}, 1000);
