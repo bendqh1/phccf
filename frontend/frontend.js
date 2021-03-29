d4d7841c09fde15e1e9bf7aea7cb3e5027f666f6
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-29 13:05:49 +0700

    Update frontend.js

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -9,30 +9,4 @@ var xhr = new XMLHttpRequest();
 xhr.open(form.method, form.action, true);
 xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
 xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-
-// Send the collected data as JSON
 xhr.send(JSON.stringify(data));
-
-// XHR passed procedure
-xhr.onloadend = function (response) {
-	if (response.target.status === 0) {
-
-		// Failed XmlHttpRequest should be considered an undefined error.
-		formStatus.className += ' alert-danger';
-		formStatus.innerHTML = form.dataset.formError;
-
-		} else if (response.target.status === 400) {
-
-			// Bad Request
-			formStatus.className += ' alert-danger';
-			formStatus.innerHTML = JSON.parse(responseText).error;
-
-		} else if (response.target.status === 200) {
-
-			// Success
-			formStatus.className += ' alert-success';
-			formStatus.innerHTML = form.dataset.formSuccess;
-
-		}
-	};
-};
