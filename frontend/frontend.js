369be373262a87c3c4bd8d3299ff7d1116578741
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-29 14:28:17 +0700

    Update frontend.js

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -7,4 +7,4 @@ var xhr = new XMLHttpRequest();
 xhr.open(contactForm.method, contactForm.action, true);
 xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
 xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-xhr.send(JSON.stringify(data));
+xhr.send();
