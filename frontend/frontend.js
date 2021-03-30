51d19af758b36be3435ac78d16eeb155f2fcb5b9
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-30 12:31:48 +0700

    Update frontend.js

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,7 +1,5 @@
 console.log("frontend.js is loaded indeed");
 
-/*
-
 let contactForm = document.querySelector("#contact_form")
 contactForm.addEventListener("submit", function(event){
 	event.preventDefault()
@@ -11,5 +9,3 @@ contactForm.addEventListener("submit", function(event){
 	xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 	xhr.send();
 });
-
-*/
