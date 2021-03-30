b2fac2021f305134d805542a331012f94ab1b3ef
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-30 15:32:10 +0700

    Update frontend.js

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -7,3 +7,17 @@ contactForm.addEventListener("submit", function(event){
 	xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 	xhr.send();
 });
+
+/*
+
+const success = document.createElement('div');
+success.setAttribute("id", "success");
+success.classList.add("general_structure", "success");
+success.innerHTML = "תודה על ההתעניינות לעבוד עימי,<br>על כל פנייה בנושאי עבודה או נגישות אשתדל להשיב בהקדם<br>בברכה,"
+
+let formParent = document.querySelector("#formParent");
+formParent.removeChild( document.querySelector("#contact_form") );
+
+formParent.appendChild(success);
+
+*/
