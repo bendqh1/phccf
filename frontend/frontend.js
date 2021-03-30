0b10ccd2a932175e0ed5e5e1e6668ba49153b03d
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-30 13:34:31 +0700

    Create frontend.js

---
+++ frontend/frontend.js
@@ -0,0 +1,11 @@
+console.log("frontend.js is loaded indeed");
+
+let contactForm = document.querySelector("#contact_form")
+contactForm.addEventListener("submit", function(event){
+	event.preventDefault()
+	var xhr = new XMLHttpRequest();
+	xhr.open(contactForm.method, contactForm.action, true);
+	xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
+	xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+	xhr.send();
+});
