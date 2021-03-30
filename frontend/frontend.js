d3f81ed4d6c0e5c517d31510f608c1eef39734c8
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-30 15:56:40 +0700

    Update frontend.js

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,22 +1,21 @@
-document.querySelector("#contact_form").addEventListener("submit", function(event) {
-	event.preventDefault()
-	var xhr = new XMLHttpRequest();
-	xhr.open(contactForm.method, contactForm.action, true);
-	xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
-	xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-	xhr.send();
-});
-
 /*
+	document.querySelector("#contact_form").addEventListener("submit", function(event) {
+		event.preventDefault()
+		var xhr = new XMLHttpRequest();
+		xhr.open(contactForm.method, contactForm.action, true);
+		xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
+		xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+		xhr.send();
+	});
 
-const success = document.createElement('div');
-success.setAttribute("id", "success");
-success.classList.add("general_structure", "success");
-success.innerHTML = "תודה על ההתעניינות לעבוד עימי,<br>על כל פנייה בנושאי עבודה או נגישות אשתדל להשיב בהקדם<br>בברכה,"
+	const success = document.createElement('div');
+	success.setAttribute("id", "success");
+	success.classList.add("general_structure", "success");
+	success.innerHTML = "תודה על ההתעניינות לעבוד עימי,<br>על כל פנייה בנושאי עבודה או נגישות אשתדל להשיב בהקדם<br>בברכה,"
 
-let formParent = document.querySelector("#formParent");
-formParent.removeChild( document.querySelector("#contact_form") );
+	let formParent = document.querySelector("#formParent");
+	formParent.removeChild( document.querySelector("#contact_form") );
 
-formParent.appendChild(success);
+	formParent.appendChild(success);
 
 */
