a29492689b260c75438294778fa7606f855ade61
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-31 01:54:29 +0700

    Update backend.php

--- backend/backend.php
+++ backend/backend.php
@@ -23,22 +23,17 @@
 			.form_output_col_1 {
 				font-weight: bold;
 			}
-			/*
-			.form_output_col_1:not(#form_output_topic):after {
-				content:": "
-			}
-			*/
 			</style>
 		</head>
 		<body dir="rtl" style="text-align: right;">
 			<div><span class="form_output_col_1">שם: </span><span class="form_output_col_2">$name</span></div>
 			<div><span class="form_output_col_1">אימייל: </span><span dir="ltr" class="form_output_col_2">$email</span></div>
 			<div><span class="form_output_col_1">טלפון: </span><span dir="ltr" class="form_output_col_2">$phone</span></div>
-			<div><span id="form_output_topic" class="form_output_col_1">נושא הפנייה: <span class="form_output_col_2">$topic</span></div>
-			<div><span class="form_output_col_1">דומיין אתר (אם יש): </span><span dir="ltr" class="form_output_col_2">$url</span></div>
-			<div><span class="form_output_col_1">יום רצוי לפגישה מרחוק (אם יש): </span><span class="form_output_col_2">$date</span></div>
-			<div><span class="form_output_col_1">שעה רצויה לפגישה מרחוק (אם יש) :</span><span class="form_output_col_2">$time</span></div>
-			<div><span class="form_output_col_1">הערות (אם יש): </span><span class="form_output_col_2">$notes</span></div>
+			<div><span class="form_output_col_1">נושא: <span class="form_output_col_2">$topic</span></div>
+			<div><span class="form_output_col_1">אתר: </span><span dir="ltr" class="form_output_col_2">$url</span></div>
+			<div><span class="form_output_col_1">תאריך: </span><span class="form_output_col_2">$date</span></div>
+			<div><span class="form_output_col_1">שעה: </span><span class="form_output_col_2">$time</span></div>
+			<div><span class="form_output_col_1">הערות: </span><span class="form_output_col_2">$notes</span></div>
 		</body>
 	</html>
 	LABEL;
