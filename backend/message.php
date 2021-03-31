c5b6d34d9fdb052ba86f7497170402747300617b
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-03-31 22:43:48 +0700

    Update message.php

--- backend/message.php
+++ backend/message.php
@@ -13,10 +13,10 @@
 			</head>
 			<body dir="rtl" style="text-align: right;">
 				<div><span class="form_output_col_1">שם: </span><span class="form_output_col_2">$name</span></div>
-				<div><span class="form_output_col_1">אימייל: </span><span dir="ltr" class="form_output_col_2">$email</span></div>
+				<div><span class="form_output_col_1">אימייל: </span><a type="email" dir="ltr" class="form_output_col_2">$email</a></div> <--! <a> -->
 				<div><span class="form_output_col_1">טלפון: </span><span dir="ltr" class="form_output_col_2">$phone</span></div>
 				<div><span class="form_output_col_1">נושא: <span class="form_output_col_2">$topic</span></div>
-				<div><span class="form_output_col_1">אתר: </span><span dir="ltr" class="form_output_col_2">$url</span></div>
+				<div><span class="form_output_col_1">אתר: </span><a type="url" dir="ltr" class="form_output_col_2">$url</a></div> <--! <a> -->
 				<div><span class="form_output_col_1">תאריך: </span><span class="form_output_col_2">$date</span></div>
 				<div><span class="form_output_col_1">שעה: </span><span class="form_output_col_2">$time</span></div>
 				<div><span class="form_output_col_1">הערות: </span><span class="form_output_col_2">$notes</span></div>
