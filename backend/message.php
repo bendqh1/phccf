2c9bed949eed42b9e506946cd1adb09d8f9f6eb3
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-04-01 01:10:01 +0700

    Update message.php

--- backend/message.php
+++ backend/message.php
@@ -13,10 +13,10 @@
 			</head>
 			<body dir="rtl" style="text-align: right;">
 				<div><span class="form_output_col_1">שם: </span><span class="form_output_col_2">$name</span></div>
-				<div><span class="form_output_col_1">אימייל: </span><span dir="ltr" href="mailto:" class="form_output_col_2">$email</span></div>
-				<div><span class="form_output_col_1">טלפון: </span><span dir="ltr" href="tel:" class="form_output_col_2">$phone</span></div>
+				<div><span class="form_output_col_1">אימייל: </span><a dir="ltr" href="mailto:" class="form_output_col_2">$email</a></div>
+				<div><span class="form_output_col_1">טלפון: </span><a dir="ltr" href="tel:" class="form_output_col_2">$phone</a></div>
 				<div><span class="form_output_col_1">נושא: <span class="form_output_col_2">$topic</span></div>
-				<div><span class="form_output_col_1">אתר: </span><span type="url" href="" dir="ltr" class="form_output_col_2">$url</span></div>
+				<div><span class="form_output_col_1">אתר: </span><a type="url" href="" dir="ltr" class="form_output_col_2">$url</a></div>
 				<div><span class="form_output_col_1">תאריך: </span><span class="form_output_col_2">$date</span></div>
 				<div><span class="form_output_col_1">שעה: </span><span class="form_output_col_2">$time</span></div>
 				<div><span class="form_output_col_1">הערות: </span><span class="form_output_col_2">$notes</span></div>
