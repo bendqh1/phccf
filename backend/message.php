c17a8ade6218f0656b2f16c656b65f8fb0acdb9e
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-04-01 18:20:39 +0700

    Update message.php

--- backend/message.php
+++ backend/message.php
@@ -16,7 +16,7 @@
 				<div><span class="form_output_col_1">אימייל: </span><a href="mailto:$email" dir="ltr" class="form_output_col_2">$email</a></div>
 				<div><span class="form_output_col_1">טלפון: </span><a href="tel:$phone" dir="ltr" class="form_output_col_2">$phone</a></div>
 				<div><span class="form_output_col_1">נושא: <span class="form_output_col_2">$topic</span></div>
-				<div><span class="form_output_col_1">אתר: </span><a href="$url" dir="ltr" class="form_output_col_2">$url</a></div>
+				<div><span class="form_output_col_1">אתר: </span><a href="$url" target="_blank" dir="ltr" class="form_output_col_2">$url</a></div>
 				<div><span class="form_output_col_1">תאריך: </span><span class="form_output_col_2">$date</span></div>
 				<div><span class="form_output_col_1">שעה: </span><span class="form_output_col_2">$time</span></div>
 				<div><span class="form_output_col_1">הערות: </span><span class="form_output_col_2">$notes</span></div>
