4c4a53f39a494c43659ff7c6c0714bfb9952f4f3
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-04-03 21:36:24 +0700

    Update message.php

--- backend/message.php
+++ backend/message.php
@@ -6,11 +6,11 @@
 				* {
 					font-size: 20px;
 				}
+				a {
+					unicode-bidi: plaintext !important;
+				}
 				.form_output_col_1 {
 					font-weight: bold;
-				}
-				#output_phone_number {
-					unicode-bidi: plaintext !important;
 				}
 				</style>
 			</head>
@@ -19,7 +19,7 @@
 				<div><span class="form_output_col_1">שם: </span><span class="form_output_col_2">$name</span></div>
 				
 				<div><span class="form_output_col_1">אימייל: </span><a dir="ltr" href="mailto:$email" class="form_output_col_2">$email</a></div>
-				<div><span class="form_output_col_1">אתר: </span><a dir="ltr" href="$url" target="_blank" id="output_phone_number" class="form_output_col_2">$url</a></div>
+				<div><span class="form_output_col_1">אתר: </span><a dir="ltr" href="$url" target="_blank" class="form_output_col_2">$url</a></div>
 				<div><span class="form_output_col_1">טלפון: </span><a dir="ltr" href="tel:$phone" class="form_output_col_2">$phone</a></div>
 				
 				<div><span class="form_output_col_1">תאריך: </span><span class="form_output_col_2">$date</span></div>
