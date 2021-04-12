82ff9b1d241a038a6a005f8e05bbbf57fba41505
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-04-12 18:56:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd WEBSITE_DIR &&
+cd WEBSITE_DIR && # Web Application Root directory comes here, instead "WEBSITE_DIR";
 rm -rf prcf-main &&
 curl -L -O https://github.com/bendqh1/prcf/archive/main.zip &&
 unzip main.zip &&
