bc9569da421ce48e185962ae70681a739997bc57
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-07-03 19:56:59 +0700

    Update contact_form.php

--- backend/contact_form.php
+++ backend/contact_form.php
@@ -1,14 +1,9 @@
 <?php
-
 	include 'include_html_fields_output_posting';
 	include 'include_recipient';
 	include 'include_subject';
 	include 'include_message_output_format.php';
 	include 'include_headers.php';
 	include 'include_mail.php';
-
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
- 		header('Location: $contact_form_success_webpage');
-		exit;
-	}
+	include 'include_success';
 ?>
