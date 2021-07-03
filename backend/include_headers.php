6ee68c3dba7c61b61b9995d541de7b0d6d4f29bf
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-07-03 20:04:41 +0700

    Update include_headers.php

--- backend/include_headers.php
+++ backend/include_headers.php
@@ -1,2 +1,2 @@
-	$headers[] = 'MIME-Version: 1.0';
-	$headers[] = 'Content-type: text/html; charset=utf-8';
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=utf-8';
