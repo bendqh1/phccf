19bd883a10e7a6c875b99807035d689f26f0f701
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-07-04 00:27:29 +0700

    Delete contact_form.php

--- backend/contact_form.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include 'include_html_fields_output_posting';
-	include 'include_recipient';
-	include 'include_subject';
-	include 'include_message_output_format.php';
-	include 'include_headers.php';
-	include 'include_mail.php';
-	include 'include_success';
-?>
