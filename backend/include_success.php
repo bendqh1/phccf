c1000692238cfc25ee0ac880ab32aaf368b087f4
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-07-03 20:03:58 +0700

    Update include_success.php

--- backend/include_success.php
+++ backend/include_success.php
@@ -1,4 +1,4 @@
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
- 		header('Location: $contact_form_success_webpage');
-		exit;
-	}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	header('Location: $contact_form_success_webpage');
+	exit;
+}
